13375d7e9e9a077357d37f462e344a4888435e17
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2017-02-01 21:25:17 -0500

    Update twitter feeds for 2016-17

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get 'about'           => 'static_pages#about'
   get 'rutinas2016'     => 'spanish2#rutinas16'
   get 'rutinas16gg'     => 'spanish2#ggspn216'
-  get 'rutinas16sg'     => 'spanish2#sgspn216'
   get 'quijote16'       => 'spanish5#quijote16'
   get 'artistas16'      => 'spanish4#artists16'
   get 'comida16'        => 'spanish3#comida16'
